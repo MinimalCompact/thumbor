c965a95d5496bf69bc595a1039d8eb4c7f5145b9
gingerlime <yoav@gingerlime.com>
2019-03-23 21:27:35 +0100

    Update imgpath.js
    * using `r.uri` directly

--- nginx-proxy/imgpath.js
+++ nginx-proxy/imgpath.js
@@ -1,7 +1,4 @@
 function file_path(r) {
-    var url = decodeURI(r.url)
-
-    var digest = require('crypto').createHash('sha1').update(url).digest('hex');
-
-    return "/" + digest.slice(NaN, 2) + "/" + digest.slice(2, 4) + "/" + digest.slice(4);
+    var digest = require('crypto').createHash('sha1').update(r.uri).digest('hex');
+    return digest.slice(NaN, 2) + "/" + digest.slice(2, 4) + "/" + digest.slice(4);
 }
