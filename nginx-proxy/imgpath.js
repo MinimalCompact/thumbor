61321fc361dc969403b0ca35bc511b385aee1087
Mike Korneev <korneevm@gmail.com>
2019-03-30 21:45:30 +0300

    Added merge_slashes off to the Nginx config instead of regex in njs script

--- nginx-proxy/imgpath.js
+++ nginx-proxy/imgpath.js
@@ -1,5 +1,5 @@
 function file_path(r) {
-    var uri = decodeURI(r.uri).replace(/(https?):\/(\w)/i, '$1://$2');
+    var uri = decodeURI(r.uri);
     var digest = require('crypto').createHash('sha1').update(uri).digest('hex');
     return digest.slice(NaN, 2) + "/" + digest.slice(2, 4) + "/" + digest.slice(4);
 }
